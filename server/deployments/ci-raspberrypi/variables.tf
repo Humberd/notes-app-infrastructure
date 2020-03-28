b71b51c96894a8429b95b8114bf4c53731ac2b14
Humberd <humberd2@gmail.com>
2020-03-28 11:42:13 +0100

    Fixed broken schema

--- server/deployments/ci-raspberrypi/variables.tf
+++ server/deployments/ci-raspberrypi/variables.tf
@@ -1,10 +1,10 @@
 variable "kubernetes_host" {
-  type = string,
+  type = string
   description = "Host of Kubernetes, for example: https://example.com:6443"
 }
 
 variable "kubernetes_cluster_ca_certificate" {
-  type = string,
+  type = string
   description = "Cluster certificate. It will be base64decode(thisToken)"
 }
 
