a0473246d451f0e24ece3745d4b50a8addecee2e
Humberd <humberd2@gmail.com>
2020-03-28 11:57:41 +0100

    Added kubernetes config for helm

--- server/deployments/ci-raspberrypi/main.tf
+++ server/deployments/ci-raspberrypi/main.tf
@@ -5,6 +5,15 @@ provider "kubernetes" {
   token = base64decode(var.kubernetes_token)
 }
 
+provider "helm" {
+  kubernetes {
+    load_config_file = false
+    host = var.kubernetes_host
+    cluster_ca_certificate = base64decode(var.kubernetes_cluster_ca_certificate)
+    token = base64decode(var.kubernetes_token)
+  }
+}
+
 module "app" {
   source = "../../schema/app"
 }
